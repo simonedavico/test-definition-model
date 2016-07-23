eb5dc9fc6db3a5c02e33f06cec584f09dcfea200
simonedavico <simonedavico@gmail.com>
2016-07-23 18:03:14 +0200

    Minor formatting fixes

--- src/main/scala/cloud/benchflow/test/config/sut/wfms/package.scala
+++ src/main/scala/cloud/benchflow/test/config/sut/wfms/package.scala
@@ -9,19 +9,27 @@ import cloud.benchflow.test.config._
   */
 package object wfms {
 
+
+
   case object WfMS extends SutsType
 
+
   case class WfMSOperation(override val name: String, override val data: Option[String]) extends Operation(name, data)
+
 
   sealed abstract class WfMSDriver(properties: Option[Properties],
                                    operations: Seq[WfMSOperation],
                                    configuration: Option[DriverConfiguration])
     extends Driver[WfMSOperation](properties, operations, configuration)
 
+
+
   case class WfMSStartDriver(override val properties: Option[Properties],
                              override val operations: Seq[WfMSOperation],
                              override val configuration: Option[DriverConfiguration])
     extends WfMSDriver(properties, operations, configuration)
+
+
 
   object WfMSDriver {
     def apply(t: String,
